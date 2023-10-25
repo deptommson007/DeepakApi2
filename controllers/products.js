919c3a5a4efc64de25c7a919437a819c2ac2e876
deptommson007 <110118190+deptommson007@users.noreply.github.com>
2023-10-25 16:27:45 +0530

    Update products.js
    add business part

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Business = require("../models/business");
 
 const getAllProducts = async (req, res) => {
 const { company, name, featured, sort, select } = req.query;
@@ -15,27 +16,36 @@ if (featured) {
    queryObject.featured = featured;
 }
 
-let apiData = Product.find(queryObject);
+let apiDataP = Product.find(queryObject);
+let apiDataB = Business.find(queryObject);
 
 if(sort) {
    let sortFix = sort.split(",").join(" ");
-   apiData = apiData.sort(sortFix);
+   apiDataP = apiDataP.sort(sortFix);
+   apiDataB = apiDataB.sort(sortFix);
 }
 if(select) {
-   // let selectFix = select.replace(",", " ");
+  
    let selectFix = select.split(",").join(" ");
-   apiData = apiData.select(selectFix);
+   apiDataP = apiDataP.select(selectFix);
+   apiDataB = apiDataB.select(selectFix);
 }
 
 let page = Number(req.query.page) || 1;
-let limit = Number(req.query.limit) || 10;
+let limit = Number(req.query.limit) || 3;
 
 let skip = (page - 1) * limit;
 
-apiData = apiData.skip(skip).limit(limit)
+apiDataP = apiDataP.skip(skip).limit(limit);
+apiDataB = apiDataB.skip(skip).limit(limit);
 
-   const Products = await apiData; // const Products = await Product.find({});
-   res.status(200).json({ Products, nbHits: Products.length });
+   const products = await apiDataP;
+    const business = await apiDataB;
+    const responseData = {
+      products: products,
+      business: business
+    };
+   res.status(200).json({responseData, nbHits: responseData.length })
 };
 
 const getAllProductsTesting = async (req, res ) => {
